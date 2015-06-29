1afeb13cd04a18978e2162e30ab00e01dfde34be
Graeme Stuart <ggstuart@dmu.ac.uk>
2015-06-29 17:14:55 +0100

    check to ensure path is to a real file

--- cegads/model_data.py
+++ cegads/model_data.py
@@ -1,16 +1,23 @@
+from cegads import CEGADSException
 from pkg_resources import resource_filename
+import os.path
 
 import pandas as pd
+
+class InvalidDataFileError(CEGADSException): pass
 
 class ModelData(object):
     """A class to represent model source data
     Can be used to manipulate data before loading it into a model
     """
-    def __init__(self, path):
+    def __init__(self, path=None):
         """load data from a file"""
         if not path:
             # Use the default data file unless an alternative file path is provided
             path = resource_filename('cegads', 'data/daily profiles.csv')
+
+        if not os.path.isfile(path):
+            raise(InvalidDataFileError("The provided path does not point to a file"))
 
         df = pd.read_csv(path, parse_dates={'Date': ['Time']})
         df = df.set_index('Date')
@@ -30,8 +37,8 @@ class ModelData(object):
         profile.name = 'profile'
         return profile
 
-    def half_hourly(self, index, method='linear'):
-        return self.interpolated(index, '30Min', method)
+    # def half_hourly(self, index, method='linear'):
+    #     return self.interpolated(index, '30Min', method)
 
 if __name__ == "__main__":
     import sys
@@ -39,30 +46,34 @@ if __name__ == "__main__":
     from matplotlib import pyplot as plt
 
     args = sys.argv
-    if len(args) != 2:
+    if len(args) >= 2:
         print("only one argument - the path to data - is allowed.")
         exit()
 
-    path = args[1]
-    if not os.path.isfile(path):
-        print("cannot find file at {}".format(path))
-        exit()
+    if len(args) == 1:
+        path = None
+    else:
+        path = args[1]
+        if not os.path.isfile(path):
+            print("cannot find file at {}".format(path))
+            exit()
+
 
     md = ModelData(path)
 
 # ['Cold Appliances', 'Cooking', 'Lighting', 'Audiovisual', 'ICT', 'Washing/ drying/ dishwasher', 'Water heating', 'Heating', 'Other', 'Unknown', 'Showers']
-    index = 'Washing/ drying/ dishwasher'
-
+    index = 'wet_appliances'
+    freq = "10Min"
     # ck1 = md.raw('Cooking')
     for method in ['linear', 'cubic', 'quadratic', 'pchip']:
-        ck = md.half_hourly(index, method=method)
-        plt.plot(ck.index, ck, label=method)
+        p = md.profile(index, freq=freq, method=method)
+        plt.plot(p.index, p, label=method)
     plt.legend(loc=2)
     plt.savefig("interpolation.png")
 
     plt.figure(2)
-    for key in md.keys():
-        ck = md.half_hourly(key, method='linear')
-        plt.plot(ck.index, ck, label=key)
+    for key in md._data.keys():
+        p = md.profile(key, freq=freq, method='linear')
+        plt.plot(p.index, p, label=key)
     plt.legend(loc=2)
     plt.savefig("models.png")
