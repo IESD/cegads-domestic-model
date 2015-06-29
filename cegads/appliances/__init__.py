8775bbcf934d599ae867561c043912c3b1c68145
Graeme Stuart <ggstuart@dmu.ac.uk>
2015-06-29 17:14:15 +0100

    default to no path

--- cegads/appliances/__init__.py
+++ cegads/appliances/__init__.py
@@ -18,7 +18,7 @@ class Appliance(object):
 class ApplianceFactory(object):
     """A thing to make appliance instances using a given model factory
     """
-    def __init__(self, path, freq="1Min", method="cubic"):
+    def __init__(self, path=None, freq="1Min", method="cubic"):
         self.model_factory = ModelFactory(path, freq, method)
 
     def __call__(self, appliance, cycle_length):
