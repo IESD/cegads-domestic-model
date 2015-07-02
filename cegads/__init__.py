ad72c9ed5c6e43f58744ba4aaff1e5189cd29c13
Graeme Stuart <ggstuart@dmu.ac.uk>
2015-07-02 15:26:31 +0100

    Extend public API

--- cegads/__init__.py
+++ cegads/__init__.py
@@ -1,5 +1,6 @@
 from .exceptions import *
 from .households import *
 from .scenarios import *
+from .appliances import *
 
-__all__ = (exceptions.__all__ + households.__all__ + scenarios.__all__)
+__all__ = (exceptions.__all__ + households.__all__ + scenarios.__all__, appliances.__all__)
--- cegads/appliances/__init__.py
+++ cegads/appliances/__init__.py
@@ -1,3 +1,5 @@
 from .model import ModelFactory
 from .appliance import Appliance
 from .factory import ApplianceFactory
+
+__all__ = ['ModelFactory', 'Appliance', 'ApplianceFactory']
